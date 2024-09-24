d03e717be5ac9c2a9e81d15619d5f446a0bafdfb
Sabique-Islam <sabiqueislam7@gmail.com>
2024-09-24 23:08:04 +0530

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import PyPDF2 as pdf
-pdfFiles = ["P1.pdf", "P2.pdf"]
+pdfFiles = ["name of pdf 1.pdf", "name of pdf 2.pdf"] # Add all the PDFs you want to merge
 merger = pdf.PdfMerger()
 for i in pdfFiles:
     pdfFile = open(i, 'rb')
     pdfReader = pdf.PdfReader(pdfFile)
     merger.append(pdfReader)
 pdfFile.close()
-merger.write("MERGED.pdf")
+merger.write("MERGED PDF.pdf")
